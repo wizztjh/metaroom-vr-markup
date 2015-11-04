70f2139a513839d2ef6284d2ace8fa88185a3cd7
SantanMaddi <saisantan2004@gmail.com>
2015-11-04 16:33:24 +0800

    [Finishes #107191088] Developer can update src of <meta-item>

--- src/meta-item.js
+++ src/meta-item.js
@@ -5,8 +5,7 @@ class MetaItemController extends MRM.MetaComponentController {
     this.parent = dom.parentElement.controller;
 
     this.metaObject = this.createMetaObject()
-
-    this.updateMetaObject()
+    this.updateMetaObject();
   }
 
   get propertiesSettings() {
@@ -15,7 +14,7 @@ class MetaItemController extends MRM.MetaComponentController {
       length: {type: Number, default: 1, attrName: 'length'},
       height: {type: Number, default: 1, attrName: 'height'},
       materialSrc: {type: String, default: '', attrName: 'material-src'},
-      geometrySrc: {type: String, default: '', attrName: 'geometry-src'}
+      src: {type: String, default: '', attrName: 'src'}
     }
   }
 
@@ -52,31 +51,88 @@ class MetaItemController extends MRM.MetaComponentController {
   }
 
   updateMetaObject(){
-    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-    var loader = new THREE.OBJMTLLoader();
+    var scope = this, material;
     // TODO: Refactor this
-    if(!this.properties.geometrySrc){
+    if(!this.properties.src){
       return;
     }
+    var url = this.properties.src;
+    var loader = THREE.Loader.Handlers.get(url);
     this.metaObject.group.position.z = this.properties.height / 2;
     if(this.metaStyle.metaStyle["position"] === 'absolute'){
       this.setAbsolutePostion();
     }
+
+    function daeLoaderCallback(collada){
+      scope.clearGroup();
+      scope.metaObject.mesh = collada.scene;
+      scope.scaleMetaObject();
+      scope.metaObject.group.add( scope.metaObject.mesh );
+    }
+
+    function objLoaderCallback(object){
+      scope.clearGroup();
+      scope.metaObject.mesh = object;
+      scope.scaleMetaObject();
+      scope.metaObject.group.add( scope.metaObject.mesh );
+    }
+
+    function plyLoaderCallback(geometry){
+      var mesh;
+      scope.clearGroup();
+      if(scope.properties.materialSrc){
+        var materialLoader = THREE.Loader.Handlers.get(scope.properties.materialSrc);
+        materialLoader.load(scope.properties.materialSrc, function(materials) {
+          var material = materials.create( object.material.name );
+					if ( material ) object.material = material;
+          scope.metaObject.mesh = new THREE.Mesh(geometry, material);
+        });
+      }else{
+        scope.metaObject.mesh = new THREE.Mesh( geometry );
+      }
+      scope.scaleMetaObject();
+      scope.metaObject.group.add( scope.metaObject.mesh );
+    }
+
+    function objmtlLoaderCallback(object){
+      scope.clearGroup();
+      scope.metaObject.mesh = object;
+      scope.scaleMetaObject();
+      scope.metaObject.group.add( scope.metaObject.mesh );
+    }
+
     //TODO: why keep loading this? Don't need to keep loading if geometrySrc and materialSrc did not change
-    loader.load( this.properties.geometrySrc , this.properties.materialSrc, ( object ) => {
-      _.forEach(this.metaObject.group.children, (child) => {
-        this.metaObject.group.remove(child);
-      });
-      this.metaObject.mesh = object
-      var bbox = new THREE.BoundingBoxHelper( this.metaObject.mesh, 0xff0000 );
-      bbox.update();
-      this.metaObject.mesh.scale.set(this.properties.width / bbox.box.size().x,
-        this.properties.height / bbox.box.size().y, this.properties.length / bbox.box.size().z);
-      this.metaObject.mesh.rotation.x = 90 * (Math.PI/180);
-      bbox.update();
-      // this.metaObject.box = bbox;
-      // this.metaObject.group.add( this.metaObject.box );
-      this.metaObject.group.add( this.metaObject.mesh );
+    if(loader instanceof THREE.ColladaLoader){
+      loader.load(url, daeLoaderCallback);
+    }
+    else if (loader instanceof THREE.PLYLoader) {
+      loader.load(url, plyLoaderCallback);
+    }
+    else if(loader instanceof THREE.OBJLoader){
+      if(scope.properties.materialSrc){
+        loader = new THREE.OBJMTLLoader();
+        loader.load(url, scope.properties.materialSrc, objmtlLoaderCallback);
+      }else{
+        loader.load(url, objLoaderCallback);
+      }
+    }
+  }
+
+  scaleMetaObject(){
+    var bbox = new THREE.BoundingBoxHelper( this.metaObject.mesh, 0xff0000 );
+    bbox.update();
+    this.metaObject.mesh.scale.set(this.properties.width / bbox.box.size().x,
+      this.properties.height / bbox.box.size().y, this.properties.length / bbox.box.size().z);
+    // this.metaObject.mesh.scale.set(this.properties.width, this.properties.height, this.properties.length);
+    this.metaObject.mesh.rotation.x = 90 * (Math.PI/180);
+    bbox.update();
+    // this.metaObject.box = bbox;
+    // this.metaObject.group.add( this.metaObject.box );
+  }
+
+  clearGroup(){
+    _.forEach(this.metaObject.group.children, (child) => {
+      this.metaObject.group.remove(child);
     });
   }
 
